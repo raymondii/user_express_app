ff8e745d681e7cdf5737dd22e593ad70683f73a5
Ray Dixon <ray@Rays-MacBook-Air.local>
2024-01-25 10:59:16 -0500

    fixed connections

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const is_prod = process.env.NODE_ENV == "production";
+// const is_prod = process.env.NODE_ENV == "production";
 
 // create connection to our mysql data base
 const connection = mysql.createConnection({
     host: process.env.DB_HOST_URL,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD || "",
-    database: process.env.mysql_first_day_db
+    database: process.env.DB_DATABASE_NAME
   });
 
 module.exports = connection.promise();
